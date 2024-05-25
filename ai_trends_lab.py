2937e27911aee321392d977484d27d80c9425f78
Mahaveer Chandel <mg1chandel@gmail.com>
2024-05-25 17:45:11 +0530

    chnages

--- ai_trends_lab.py
+++ ai_trends_lab.py
@@ -9,6 +9,8 @@ Original file is located at
 
 import pandas as pd
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
